9938c7c9515f93a22a701a9db6af4599b9d4099f
Balakumaran Sugumar <sbkumaran230986@gmail.com>
2019-06-16 10:22:37 -0400

    new checks

--- src/com/python/balakumaran/FirstApplication.py
+++ src/com/python/balakumaran/FirstApplication.py
@@ -3,3 +3,39 @@
 this_is_tamil = 'பாலகுமாரன் சுகுமார்'
 print(this_is_tamil)
 
+the_world_is_flat = True
+if the_world_is_flat:
+    print('Better not to fall of the cliff')
+
+# classic division returns float
+float_number = 17/3
+print(float_number)
+
+# floor division only returns the fractional section
+floor_number = 17 // 3
+print(floor_number)
+
+# modules is the same as that of Java
+modules_number = 17 % 3
+print(modules_number)
+
+# square of a number
+squared_number = 5 ** 2
+print(squared_number)
+
+# power of a number
+powered_number = 2 ** 7
+print(powered_number)
+
+# if you dont want the interpreter to handle / as special character
+non_escaped_character = '(c:/test/python/interpreter)'
+print(non_escaped_character)
+
+# print the number of instances before the string
+num_times = 3 * 'um' + 2 * 'test'
+print(num_times)
+
+
+
+
+
