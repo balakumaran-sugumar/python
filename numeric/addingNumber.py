b9faf7a04db20b32994aa67f01ffcd8933ee8f03
balakumaran-sugumar <kumaransugumar@Kumarans-MacBook-Pro.local>
2020-02-12 23:29:28 -0500

    adding additional files

---
+++ numeric/addingNumber.py
@@ -0,0 +1,8 @@
+import math
+
+firstNumber = input("Enter the first Number: ")
+secondNumber = input("Enter the second Number: ")
+
+addition = int(firstNumber) + int(secondNumber)
+
+print('addintion of two numbers ',addition)
---
+++ stringExample.py
@@ -0,0 +1,5 @@
+firstName = input('What is the first name ')
+
+lastName = input('What is the last name ')
+
+print (firstName.capitalize() + ' ' + lastName.capitalize())
