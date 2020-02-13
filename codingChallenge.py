d2bfed25edb9256ad62e7fa0b559f2a4137cd720
balakumaran-sugumar <kumaransugumar@Kumarans-MacBook-Pro.local>
2020-02-12 22:42:12 -0500

    adding challenges

---
+++ codingChallenge.py
@@ -0,0 +1,11 @@
+#print('why won't this code print')
+#solution
+print('why won\'t this code print')
+
+#pirnt('this line fails too!')
+print ('this line fails too !')
+
+#print 'I know how to fix this one'
+print ('I know how to fix this one')
+
+
