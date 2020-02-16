f589b3fc1e09fcbfccbf876a82d983cbb8478a18
kumaransugumar <sbkumaran230986@gmail.com>
2020-02-15 19:46:46 -0500

    adding number and functions

--- numeric/addingNumber.py
+++ numeric/addingNumber.py
@@ -1,8 +1,32 @@
+
+# importing for python mathematics functions
 import math
 
+# taking the number from the user
 firstNumber = input("Enter the first Number: ")
 secondNumber = input("Enter the second Number: ")
 
+# adding the two number
 addition = int(firstNumber) + int(secondNumber)
 
-print('addintion of two numbers ',addition)
+# adding the two number
+print('addition of two numbers ', addition)
+
+# modulus operator: this is going to give the reminder of the operation
+print("Modular operator : " + str(10 % 3))
+
+# converting number into string
+print("The addition of the two numbers : " + str(addition))
+
+# converts the number into a absolute number
+abs_number = -5
+print("Abs of a number " + str(abs(abs_number)))
+
+# power function
+pow_number = pow(2, 5)
+print("Power of 2 ^ 5 : " + str(pow_number))
+
+# using some of the mathematics functions
+print("Rounding the value : " + str(round(3.4)))
+
+
--- print_string/functionIntro.py
+++ print_string/functionIntro.py
@@ -1,7 +1,16 @@
+# defining a function that will take in a user-address and pin
 def addAddress(userAddress, pin):
-  print('Added the new address : ',userAddress)
+    print('Added the new address : ', userAddress)
 
+
+# this input user-address and pin
 userAddress = 'WashingtonDC USA'
 pin = '123456'
 
+# calling a function
 addAddress(userAddress, pin)
+
+# this will give the the index value of the word U
+print(userAddress.index("U"))
+# this will replace the character with blank space
+print(userAddress.replace("A", ""))
